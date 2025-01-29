b7e8352a3dbe4ff378b05ac149ee14bd17d494ff
Virupakshi047 <144578836+Virupakshi047@users.noreply.github.com>
2025-01-29 21:20:46 +0530

    Update swap.sh

--- swap.sh
+++ swap.sh
@@ -5,10 +5,10 @@ echo "Enter second  number"
 read b
 echo "before swapping"
 echo $a
-echo $b         # swapping logic
-b=$((a+b))       # b has now a+b
-a=$((b-a))       # separating b from a+b(-a)
-b=$((b-a))       # now a act as b value separating a that will give back b
+echo $b         
+b=$((a+b))       
+a=$((b-a))       
+b=$((b-a))       
 echo "swappwed numbers are"
 echo $a
 echo $b
